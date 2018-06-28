17cd7ed559649da3e5cdc43984a28afb4550df35
Kiran <kiranprajapati2719@gmail.com>
2018-06-28 18:59:29 +0545

    HELLO WORLD

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
-import AppRouter from "./routers/AppRouter";
-
-ReactDOM.render(AppRouter, document.getElementById("root"));
-registerServiceWorker();
